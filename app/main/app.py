94355c891a3b7b0d38c025f4a3b49883371f9b0d
Hannes Ulrich <hannes.ulrich@uksh.de>
2024-02-12 16:29:55 +0100

    Adding dynamic topic display on the index page

--- app/main/app.py
+++ app/main/app.py
@@ -3,17 +3,26 @@ from flask import Flask, render_template, Response
 import logic.consumeAndProcess
 import logic.createImage
 import fhirizer.createRessource
+from confluent_kafka import Consumer, KafkaError, KafkaException
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    # Execute the shell script and get output
-    result = subprocess.run(['app/scripts/get_topics.sh'], stdout=subprocess.PIPE, text=True)
-    # Split the output into a list of topics
-    topics = result.stdout.splitlines()
+    conf = {'bootstrap.servers': '172.26.241.166:9092',
+            'group.id': 'mice',
+            'enable.auto.commit': 'false',
+            'auto.offset.reset': 'latest'}
+    consumer = Consumer(conf)
+    temp = consumer.list_topics().topics.keys()
+    topics = []
+    for t in temp:
+        if t.startswith('sdc'):
+            topics.append(t)
     # Render these topics in the index.html template
     return render_template('index.html', topics=topics)
+
 
 # Other routes and functions...
 
@@ -21,18 +30,19 @@ def index():
 def process_topic(topic_name):
     topics = [topic_name]
     labels, data, record = logic.consumeAndProcess.runlogic(topics)
-    #Here FHIR Stuff
+    # Here FHIR Stuff
     base = logic.createImage.createImage(record)
     fhirizer.createRessource.createBinaryRessource(base)
     # Here, call the function that processes the topic
     # For demonstration, this will just return a simple message
     # In real use, you might redirect to another page that shows the processing result or status
-    #return f"Processing topic: {type(recordedstack)}"
+    # return f"Processing topic: {type(recordedstack)}"
     return render_template(
         template_name_or_list='chartjs-example.html',
         data=data,
         labels=labels,
     )
+
 
 if __name__ == '__main__':
     app.run(debug=True)
